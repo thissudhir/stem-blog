a400de530a3a7312bc3066b843b21177f4907b13
Abhishek Yadav <abhishekpsitfor4years@gmail.com>
2023-09-02 14:27:14 +0530

    update env

--- backend/db.js
+++ backend/db.js
@@ -6,9 +6,9 @@ dotenv.config();
 
 
 export const db= mysql.createConnection({
-    host:process.env.host,
-    user: process.env.user,
-    password:process.env.password,
-    database: process.env.database,
-    port: process.env.port,
+    host:process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password:process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT,
 })
