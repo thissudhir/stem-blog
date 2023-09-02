57b07467dde9e44c7ef410f274ec832993ff849d
Abhishek Yadav <abhishekpsitfor4years@gmail.com>
2023-09-02 13:12:06 +0530

    update port

--- backend/db.js
+++ backend/db.js
@@ -3,10 +3,12 @@ import dotenv from 'dotenv';
 
 
 dotenv.config();
+// mysql://root:mbXw7CkLPxzmcoIEIG8m@containers-us-west-81.railway.app:7942/railway
 
 export const db= mysql.createConnection({
     host:process.env.host,
     user: process.env.user,
     password:process.env.password,
     database: process.env.database,
+    port: process.env.port,
 })
